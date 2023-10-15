e4de24b932f04ed0a92a6650b20247a0108d66bc
@jeric.adriano <jeric.adriano@nutricoach.pro>
2023-10-15 20:41:46 +0800

    Add edit/update article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,23 +1,39 @@
 class ArticlesController < ApplicationController
- def show
-  @article = Article.find(params[:id])
- end
-
- def index
-  @articles = Article.all
- end
-
- def new
-  @article = Article.new
- end
-
- def create
-  @article = Article.new(params.require(:article).permit(:title, :description))
-  if @article.save
-    flash[:success] = "Article successfully created"
-    redirect_to @article
-  else
-    render :new, status: :unprocessable_entity
+  def show
+    @article = Article.find(params[:id])
   end
- end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+
+    if @article.save
+      flash[:success] = "Article successfully created"
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:success] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 end
